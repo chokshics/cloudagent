91964329914c164b06c00054919a8845586cd077
chokshics <chokshics@gmail.com>
2025-08-05 16:43:34 +0530

    commit

--- client/src/pages/NewHomePage.js
+++ client/src/pages/NewHomePage.js
@@ -133,7 +133,7 @@ const NewHomePage = () => {
       {/* Header */}
       <header className="bg-white shadow-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center py-2">
+          <div className="flex justify-between items-center">
             <div className="flex items-center space-x-3">
               <img 
                 src="/logo.png?v=2" 
