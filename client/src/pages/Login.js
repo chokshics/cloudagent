cbe6a53a2b474b1ac67c841520853fb3862bbd86
chokshics <chokshics@gmail.com>
2025-09-04 20:42:34 +0530

    commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,7 +43,7 @@ const Login = () => {
             <Lock className="h-6 w-6 text-indigo-600" />
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Admin Portal Login
+            Merchant Portal Login
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Sign in to access your dashboard
